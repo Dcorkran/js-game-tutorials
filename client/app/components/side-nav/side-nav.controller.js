b654af043046664f50e82282ffc7ede52d37adbc
Dillon Corkran <dcorkran972@gmail.com>
2017-01-26 15:32:18 -0700

    :sparkles: side-nav displays current page appropriately

--- client/app/components/side-nav/side-nav.controller.js
+++ client/app/components/side-nav/side-nav.controller.js
@@ -1,10 +1,11 @@
 angular
   .module('app')
-  .controller('SideNavController',function(TutorialService){
+  .controller('SideNavController',function(TutorialService,$stateParams){
     const vm = this;
-
+    console.log($stateParams);
     vm.$onInit = function(){
-      vm.isSelected = TutorialService.isSelected;
+      vm.isSelected = $stateParams.name;
+      console.log(vm.isSelected);
     },
     vm.changeSelected = function(name){
       TutorialService.changeSelected(name);
