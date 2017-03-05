9e20642c2707d0b2a11783ebfb9419094be776d5
Dillon Corkran <dcorkran972@gmail.com>
2017-03-05 16:16:42 -0700

    :sparkles: linked to heroku

--- client/app/tutorial-page/tutorial-page.service.js
+++ client/app/tutorial-page/tutorial-page.service.js
@@ -3,7 +3,7 @@ angular
   .service('TutorialService',function($http){
     this.isSelected = 'Home';
     this.getTutorial = function(name){
-      return $http.get(`http://localhost:5000/api/v1/tutorial/${name}`);
+      return $http.get(`https://jsgtserver.herokuapp.com/api/v1/tutorial/${name}`);
     };
     this.changeSelected = function(name){
       this.isSelected = name;
